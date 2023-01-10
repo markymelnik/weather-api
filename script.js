6cf49b37a82af9c72f3c6edad666f38b13474148
markymelnik <markemelnik@gmail.com>
2023-01-10 15:55:35 -0500

    Additional weather conditions displayed from API; formatting edits

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function displayWeather(element) {
   
   const search = document.querySelector('#search');
   const temperature = document.querySelector('.temp');
+  const feelsLike = document.querySelector('.feelslike');
+  const condition = document.querySelector('.condition');
+  const wind = document.querySelector('.wind');
+  const location = document.querySelector('.location');
 
+  
   let searchValue = search.value;
 
   element.preventDefault();
@@ -21,7 +26,13 @@ function displayWeather(element) {
       const weatherResponse = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+coorData[0].lat+'&lon='+coorData[0].lon+'&appid=a514c3b8912742c6fbbe4b4f27cb598c');
       const weatherData = await weatherResponse.json();
       
-      temperature.textContent = tempConverter.toCelsius(weatherData.main.temp);
+      location.textContent = `Current weather conditions in ${(searchValue)}`;
+      temperature.textContent = `Temperature: ${tempConverter.toCelsius(weatherData.main.temp)}°C`;
+      feelsLike.textContent = `Feels Like: ${tempConverter.toCelsius(weatherData.main.feels_like)}°C`;
+      condition.textContent = `Conditions: ${(weatherData.weather[0].description)}`;
+      wind.textContent = `Wind Speed: ${weatherData.wind.speed} m/s`;
+
+      search.value = '';
       
     } catch (err) {
       console.log(err);
@@ -31,12 +42,12 @@ function displayWeather(element) {
 
 const tempConverter = (() => {
 
-  const toCelsius = (temp) => {
-    return (temp - 273.15).toFixed(2);
+  const toCelsius = (kelvin) => {
+    return (kelvin - 273.15).toFixed(0);
   }
 
-  const toFahrenheit = (temp) => {
-    return ((temp - 273.15) * (9/5) + 32).toFixed(2);
+  const toFahrenheit = (kelvin) => {
+    return ((kelvin - 273.15) * (9/5) + 32).toFixed(0);
   }
 
   return {
